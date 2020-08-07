d870d17375b1391252ca9902394cdedd57add483
rafael <rafael.sterzinger@gmx.at>
2020-08-08 01:50:36 +0200

    Finish chapter exploration

--- src/run.py
+++ src/run.py
@@ -183,7 +183,7 @@ def add_rollout_params(parser):
 
 
 def add_experiment_params(parser):
-    parser.add_argument('--env', help='environment ID', default='BreakoutNoFrameskip-v4',
+    parser.add_argument('--env', help='environment ID', default='MontezumaRevengeNoFrameskip-v4',
                         type=str)
     parser.add_argument('--seed', help='RNG seed', type=int, default=0)
     parser.add_argument('--feat_learning', type=str, default="none",

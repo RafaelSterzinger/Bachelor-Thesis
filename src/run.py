110740a77429a46923e9e1b9cff36bd4725d27e0
rafael <rafael.sterzinger@gmx.at>
2020-08-09 01:11:28 +0200

    Add acknowledgement; Start chapter implementation

--- src/run.py
+++ src/run.py
@@ -14,7 +14,6 @@ from baselines import logger
 from baselines.bench import Monitor
 from baselines.common.atari_wrappers import NoopResetEnv, FrameStack
 from mpi4py import MPI
-from datetime import datetime
 
 from auxiliary_tasks import FeatureExtractor, InverseDynamics, VAE, JustPixels
 from cnn_policy import CnnPolicy
